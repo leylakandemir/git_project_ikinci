da6cf099a58c5396596e80f8c4ecbd07528fefb6
leylakandemir <62206209+leylakandemir@users.noreply.github.com>
2020-08-10 23:45:38 +0200

    Update Deneme.java

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -4,6 +4,7 @@ public class Deneme {
 	public static void main(String[]args) {
 		System.out.println("Ikinci git projem");
 		System.out.println("ikinci satir kodum");
+		System.out.println("Githubda degisiklik yaptim");
 	}
 
 }
